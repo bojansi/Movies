58a016607711104dba266297a779a3e4eb7a51ae
bojansi <b.simic998@gmail.com>
2021-12-23 10:19:39 +0100

    Dodata klasa za rad sa tabelom Grading.

---
+++ Movies/DataAccessLayer/GradingRepository.cs
@@ -0,0 +1,82 @@
+﻿using DataAccessLayer.Modals;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    public class GradingRepository
+    {
+        //getAllGradings from Table
+        public List<Grading> GetAllGradings()
+        {
+            List<Grading> gradings = new List<Grading>();
+
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "SELECT * FROM Grading";
+
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    Grading gradin = new Grading();
+                    gradin.Id = sqlDataReader.GetInt32(0);
+                    gradin.Rating = sqlDataReader.GetDecimal(1);
+                    gradin.User_Id = sqlDataReader.GetInt32(2);
+                    gradin.Movie_Id = sqlDataReader.GetInt32(3);
+
+                    gradings.Add(gradin);
+                }
+            }
+            return gradings;
+        }
+
+        //insert new Grading
+        public int InsertGrading(Grading g)
+        {
+            using(SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = string.Format("INSERT INTO Grading VALUES ('{0}', '{1}', '{2}')", g.Rating, g.User_Id, g.Movie_Id);
+                
+                sqlConnection.Open();
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+
+        //update Grading
+        public int UpdateGrading(Grading g)
+        {
+            using(SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = string.Format("UPDATE Grading Rating='{0}', User_Id='{1}', Movie_Id='{3}' WHERE Id='{4}'", g.Rating, g.User_Id, g.Movie_Id, g.Id);
+
+                sqlConnection.Open();
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+
+        public int DeleteGrading(int Id)
+        {
+            using(SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = string.Format("DELETE FROM Grading WHERE Id='{0}'", Id);
+                
+                sqlConnection.Open();
+
+                return sqlCommand.ExecuteNonQuery();
+                
+            }
+        }
+    }
+}
--- Movies/DataAccessLayer/Models/Grading.cs
+++ Movies/DataAccessLayer/Models/Grading.cs
@@ -8,8 +8,8 @@ namespace DataAccessLayer.Modals
     {
         public int Id { get; set; }
         public decimal Rating { get; set; }
-        public User User_Id { get; set; }
-        public Movie Movie_Id { get; set; }
+        public int User_Id { get; set; }
+        public int Movie_Id { get; set; }
 
     }
 }
