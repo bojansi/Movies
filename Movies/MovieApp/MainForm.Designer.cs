d8867003da448492ba48974ddd7439439fbf9ce9
bojansi <b.simic998@gmail.com>
2021-12-24 00:39:34 +0100

    Kreirana glavna Forma i na nju dodat listBox na koji su dovuceni filmovi iz baze.

---
+++ Movies/MovieApp/MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace MovieApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.listBoxMovies = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBoxMovies
+            // 
+            this.listBoxMovies.FormattingEnabled = true;
+            this.listBoxMovies.ItemHeight = 20;
+            this.listBoxMovies.Location = new System.Drawing.Point(245, 35);
+            this.listBoxMovies.Name = "listBoxMovies";
+            this.listBoxMovies.Size = new System.Drawing.Size(671, 284);
+            this.listBoxMovies.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(928, 531);
+            this.Controls.Add(this.listBoxMovies);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Filmovi";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxMovies;
+    }
+}
---
+++ Movies/MovieApp/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLogicLayer;
+using DataAccessLayer.Modals;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MovieApp
+{
+    public partial class MainForm : Form
+    {
+        private readonly MovieBusiness movieBusiness;
+        public MainForm()
+        {
+            this.movieBusiness = new MovieBusiness();
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            List<Movie> movies = this.movieBusiness.GetAllMovies();
+
+            foreach (Movie m in movies)
+            {
+                listBoxMovies.Items.Add(m.Id + ". " + m.Name + " " + m.Year + " " + m.Genre + " " + m.Favorite + " " + m.IMDBLink + " " + m.Description);
+            }
+        }
+
+    }
+}
