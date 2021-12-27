d739ef077139b2d9dcce1dfaaf504be8dfec26cc
bojansi <b.simic998@gmail.com>
2021-12-28 00:32:01 +0100

    Dodavanje filmova preko sql upita u tableu Movies.

---
+++ Movies/SQLQuery/SQLQueryINSERTMovies.sql
@@ -0,0 +1,20 @@
+﻿begin;
+
+INSERT INTO Movies
+VALUES('Mi nismo anđeli', 1992, 'Komedija', 'true', 'https://www.imdb.com/title/tt0104860/?ref_=nv_sr_srsg_0', 'Mi nismo anđeli srbijanska/jugoslavenska je filmska komedija iz 1992. godine koju je režirao i napisao Srđan Dragojević. Glavne uloge tumače Nikola Kojo, Milena Pavlović, Uroš Đurić, Srđan Todorović, Zoran Cvijanović i Branka Katić. Postala je jedna od najpopularnijih filmova na području bivše Jugoslavije u 1990-ima. Radnja se odvija oko Anđela i Vraga koji se bore za dušu zavodnika Nikole koji ne zna da je Marina, njegova cura za jednu noć s kojom je bio dok se napio, slučajno ostala trudna. Kritičari su hvalili film zbog inventivne režije, brze montaže, urbanog humora i velikog broja referenci na pop kulturu.');
+
+INSERT INTO Movies
+VALUES('Podzemlje', 1995, 'Drama', 'false', 'https://www.imdb.com/title/tt0114787/?ref_=nv_sr_srsg_0', 'Podzemlje (engl. Undergound — Andergraund; znan i kao Bila jednom jedna zemlja) je crnohumorna antiratna drama iz 1995. koju je režirao Emir Kusturica u srpsko-francusko-njemačko-mađarskoj koprodukciji. Sam film traje 3 sata, a kasnije je skovan u 5-satnu redateljsku verziju u obliku mini-serije pod naslovom "Bila jednom jedna zemlja". Radnja se odvija oko skupine ljudi koja se skriva i živi u podzemlju od 1940-ih do 1990-ih jer misli da Drugi svjetski rat nikada nije završio zbog varke jednog od njihovih prijatelja (Miki Manojlović).');
+
+INSERT INTO Movies
+VALUES('Nataša', 2001, 'Drama', 'false', 'https://www.imdb.com/title/tt0283504/?ref_=nv_sr_srsg_0', 'Nataša je jugoslavenski film, snimljen 2001. godine, na filmu, u režiji Ljubiše Samardžića, poznat kao jedan od prvih napravljenih nakon pada režima Slobodana Miloševića, odnosno prvi koji kao deo zapleta koriste te nedavne događaje.');
+
+INSERT INTO Movies
+VALUES('Mala noćna muzika', 2002, 'Komedija', 'false', 'https://www.imdb.com/title/tt0331282/?ref_=nv_sr_srsg_0', 'Dva najbolja prijatelja rade u mrtvačnici i deluju veoma ograničeno. Život im se menja kada se javljaju na mobilni jednog mrtvaca. Oni pokušavaju da igraju igru u kojoj se ne snalaze baš najbolje, jer je to za njih veliki zalogaj, naročito kada se umešaju ozbiljni ljudi iz sveta kriminala i porno industrije. Izlazeći iz avanture u avanturu, oni sreću mnoge bizarne likove tokom nekoliko neispavanih noći u konstantnom begu, pokušavajući nekako da i oni profitiraju iz cele situacije.');
+
+INSERT INTO Movies
+VALUES('Karaula', 2006, 'Ratni', 'false', 'https://www.imdb.com/title/tt0466561/?ref_=nv_sr_srsg_0', 'Radnja filma se odvija 1987. godine[2][n 1] u karauli, na jugoslovensko-albanskoj granici. Svakodnevni vojnički život narušen je neobičnim okolnostima. Frustrirani i uvek pijan poručnik Safet Pašić oseća čudan bol u preponama. U pomoć zove jedinog lekara među vojnicima Sinišu Siriščevića koji u potpunoj diskreciji utvrđuje da se radi o polnoj bolesti — sifilisu. Ne želeći da njegova supruga sazna šta se dogodilo i tražeći razlog da ne ode kući, Pašić objavljuje vanredno stanje, tvrdeći da se albanska vojska grupiše pred karaulom. Lakrdija se pretvara u ratnu psihozu: vojnici kopaju rovove, Pašić je iz dana u dan sve nervozniji, Siniša se upušta u opasnu ljubavnu avanturu sa poručnikovom ženom, a njegov najbolji prijatelj Ljuba Paunović bezuslovno odlučuje da napusti vojsku. Situacija polako izmiče kontroli.');
+
+INSERT INTO Movies
+VALUES('Do koske', 1997, 'Drama', 'false', 'https://www.imdb.com/title/tt0188568/?ref_=nv_sr_srsg_0', 'Do koske je srpski igrani film snimljen u režiji Slobodana Skerlića i premijerno prikazan 1997. godine. Scenario je napisao Srđa Anđelić. Poznat je po izuzetno eksplicitnim scenama nasilja i crnom humoru, ali i izuzetno kritičnim prikazom ekonomske i kulturne situacije u Srbiji pod tadašnjom Miloševićevom vlašću.');
+end;
