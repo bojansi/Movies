6471101bb489ae00ddbbc18bf078fed787ce6fe8
bojansi <b.simic998@gmail.com>
2021-12-29 02:09:14 +0100

    Dodavanje novih zvezcia za ocenjivanje filma.

--- Movies/MovieApp/Properties/Resources.Designer.cs
+++ Movies/MovieApp/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace MovieApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap LoginPicture {
+            get {
+                object obj = ResourceManager.GetObject("LoginPicture", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap white_star {
             get {
                 object obj = ResourceManager.GetObject("white_star", resourceCulture);
